90b1b7b995d45e4db9a28deb7245affeccf6061c
Suraj Kumar Jha <surajkumarjha771@gmail.com>
2026-09-04 23:02:51 +0530

    Redis connection factory bean setup

--- src/main/java/com/swiftride/paymentservice/Configs/RedisConfig.java
+++ src/main/java/com/swiftride/paymentservice/Configs/RedisConfig.java
@@ -1,14 +1,32 @@
 package com.swiftride.paymentservice.Configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
+    @Value("${REDIS_HOST:localhost}")
+    private String REDIS_HOST;
+
+    @Value("${REDIS_PORT:6379}")
+    private int REDIS_PORT;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(REDIS_HOST);
+        config.setPort(REDIS_PORT);
+
+        return new LettuceConnectionFactory(config);
+    }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
