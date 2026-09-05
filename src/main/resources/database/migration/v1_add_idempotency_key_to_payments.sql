848c525dbe28e0f357fbe98fc982af0eb8a1211b
Suraj Kumar Jha <surajkumarjha771@gmail.com>
2026-09-05 19:50:12 +0530

    added flyway to support db migrations

---
+++ src/main/resources/database/migration/v1_add_idempotency_key_to_payments.sql
@@ -0,0 +1,6 @@
+ALTER TABLE payments ADD COLUMN "idempotencyKey" VARCHAR(255);
+
+UPDATE payments SET "idempotencyKey" = "legacy-" || id WHERE "idempotencyKey" IS NULL;
+
+ALTER TABLE payments ALTER COLUMN "idempotencyKey" SET NOT NULL;
+ALTER TABLE payments ADD CONSTRAINT uq_idempotency_key UNIQUE ("idempotencyKey");
